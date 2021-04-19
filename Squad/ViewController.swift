79c11f2dc24016fd7f12d8cfba4545797e6dfbe7
Christian Stiker <cstiker05@gmail.com>
2021-04-18 20:43:48 -0400

    Initial Commit

---
+++ Squad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Squad
+//
+//  Created by Christian Stiker on 4/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
